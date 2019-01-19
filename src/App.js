6c39103ad20d7a13e3ce2546b4979890996cb0ab
rmcc0724 <rmcc0724@gmail.com>
2019-01-19 12:27:01 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,64 @@
-import React from 'react'
-import CoinTable from './components/coin-table'
-
-import data from './data/coins.json'
-
-import './app.css'
+import React from "react";
+import CoinTable from "./components/coin-table";
+import data from "./data/coins.json";
+import "./app.css";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       data: data,
       direction: {
-        price_usd: 'asc',
+        price_usd: "asc",
+        symbol: "asc"
       }
-    }
-
-    this.sortBy = this.sortBy.bind(this)
+    };
+    this.sortByNum = this.sortByNum.bind(this);
+    this.sortByName = this.sortByName.bind(this);
   }
-
-  sortBy(key) {
+  sortByNum(key) {
     this.setState({
-      data: data.sort( (a, b) => (
-        this.state.direction[key] === 'asc'
+      data: data.sort((a, b) =>
+        this.state.direction[key] === "asc"
           ? parseFloat(a[key]) - parseFloat(b[key])
-          : parseFloat(b[key]) - parseFloat(a[key]) 
-      )),
+          : parseFloat(b[key]) - parseFloat(a[key])
+      ),
       direction: {
-        [key]: this.state.direction[key] === 'asc'
-          ? 'desc'
-          : 'asc'
+        [key]: this.state.direction[key] === "asc" ? "desc" : "asc"
       }
-    })
+    });
+    console.log("SortByNum");
   }
-
+  sortByName(key) {
+    this.setState({
+      data: data.sort((a, b) =>
+        this.state.direction[key] === "asc"
+          ? a[key] < b[key]
+            ? -1
+            : a[key] > b[key]
+            ? 1
+            : 0
+          : a[key] > b[key]
+          ? -1
+          : a[key] < b[key]
+          ? 1
+          : 0
+      ),
+      direction: {
+        [key]: this.state.direction[key] === "asc" ? "desc" : "asc"
+      }
+    });
+  }
   render() {
     return (
-      <div
-        className="page-container"
-      >
+      <div className="page-container">
         <CoinTable
           data={this.state.data}
-          sortBy={this.sortBy}
+          sortByNum={this.sortByNum}
+          sortByName={this.sortByName}
         />
       </div>
-    )
+    );
   }
 }
-
-export default App
+export default App;
